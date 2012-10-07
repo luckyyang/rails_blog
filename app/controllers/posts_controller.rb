d2303846752674da6b73f607658fa920f23540f1
Liu Jiyang <2411mail@gmail.com>
2012-10-07 15:33:24 +0800

    complete basic blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with :name => "liujiyang", :password => "jiyangliu", :except => [:index, :show]
   # GET /posts
   # GET /posts.json
   def index
